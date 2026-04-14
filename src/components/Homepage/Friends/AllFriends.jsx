6db4fc9f759b9762070794641ebcc5d4bc3c5be7
Kz-Himel <kzhimel129@gmail.com>
2026-04-14 20:35:53 +0600

    fetch friend Data from friendsData.json

--- src/components/Homepage/Friends/AllFriends.jsx
+++ src/components/Homepage/Friends/AllFriends.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { use } from 'react';
+import FriendsCard from './FriendsCard'
+
+const friendsPromise = async () => {
+    const res = await fetch('friendsData.json');
+    const data = await res.json();
+}
 
 const AllFriends = () => {
+
+    const friends = use(friendsPromise);
+
     return (
         <div>
-            
+            <h2>Your Friends</h2>
+
+            <section>
+                {
+                    friends.map((friend, id) => (
+                        <FriendsCard key={id} friend={friend}/>
+                    ))
+                }
+            </section>
         </div>
     );
 };
