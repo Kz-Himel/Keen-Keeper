764af8d5ca7e71f7267319aa8c9fe5c3bd720346
Kz-Himel <kzhimel129@gmail.com>
2026-04-16 19:02:08 +0600

    Added a loader animation in data fetching component [AllFriends]

--- src/components/Homepage/Friends/AllFriends.jsx
+++ src/components/Homepage/Friends/AllFriends.jsx
@@ -1,12 +1,41 @@
-import React, { use } from 'react';
+import React, { useEffect, useState } from 'react';
 import FriendsCard from './FriendsCard'
-
-const friendsPromise = fetch('/data/friendsData.json')
-  .then(res => res.json());
 
 const AllFriends = () => {
 
-    const friends = use(friendsPromise);
+    const [friends, setFriends] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('/data/friendsData.json')
+            .then(res => res.json())
+            .then(data => {
+                setFriends(data);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="flex flex-col justify-center items-center h-[60vh] gap-4">
+            
+            {/* Animated Ring */}
+            <span className="loading loading-ring loading-lg text-primary"></span>
+
+            {/* App Name */}
+            <h2 className="text-xl font-semibold tracking-wide">
+                KeenKeeper
+            </h2>
+
+            {/* Subtext */}
+            <p className="text-sm text-gray-500 animate-pulse">
+                Loading your friends...
+            </p>
+
+        </div>
+        );
+    }
+
 
     return (
         <div className="container mx-auto px-4 pt-24 mb-10">
